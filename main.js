e46e3fb162f943891694b1b18bcfde0ffaae7596
arquer <arquerr92@gmail.com>
2023-10-17 16:56:34 -0300

    imagen..

--- main.js
+++ main.js
@@ -28,7 +28,7 @@ function cargarProductos(productosElegidos) {
         div.classList.add("producto");
         div.innerHTML = `
         
-                    <img class="producto-imagen" src="${producto.imagen}" alt="${producto.titulo}">
+                    <img class="producto-imagen" src="./assets/img/${producto.imagen}" alt="${producto.titulo}">
                     <div class="producto-detalles">
                         <h3 class="producto-titulo">${producto.titulo}</h3>
                         <p class="producto-precio">$${producto.precio}</p>
